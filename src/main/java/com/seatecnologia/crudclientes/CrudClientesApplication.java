6da6caaeab6b17382900c0442ec94f6ba1aac374
Jackson Leste <jacksongoncalves.leste@gmail.com>
2025-01-13 14:31:02 -0300

    feat: adicionar mensagem de inicio do servidor spring

--- src/main/java/com/seatecnologia/crudclientes/CrudClientesApplication.java
+++ src/main/java/com/seatecnologia/crudclientes/CrudClientesApplication.java
@@ -1,13 +1,19 @@
 package com.seatecnologia.crudclientes;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class CrudClientesApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(CrudClientesApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CrudClientesApplication.class, args);
+		logger.warn("\nO Servidor Spring Boot está rodando!\n");
 	}
 
 }
